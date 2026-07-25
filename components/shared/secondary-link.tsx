97902ef1e2ac662dccd6e7cb6f5f8fe12068ee07
Abdul Karim <karim.develops@gmail.com>
2026-07-25 14:05:32 +0500

    feat: create secondary link component

---
+++ components/shared/secondary-link.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function SecondaryLink({
+  link,
+  text,
+}: {
+  link: string;
+  text: string;
+}) {
+  return (
+    <Link href={link} className="font-semibold text-primary">
+      {text}
+    </Link>
+  );
+}
