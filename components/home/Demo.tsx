c55b7a2db25a5d5e1391dcaf27ebd0434b3df10e
Abdul Karim <karim.develops@gmail.com>
2026-07-25 22:24:18 +0500

    feat: add a demo section

---
+++ components/home/Demo.tsx
@@ -0,0 +1,28 @@
+export default function Demo() {
+  return (
+    <div className="flex items-center justify-center gap-40 rounded-t-4xl bg-muted-primary p-5 md:p-10">
+      <div className="-rotate-10 rounded-4xl bg-background p-5 font-bold">
+        Placholder
+        <br /> Blah blah blah
+        <br /> Yada Yada Yada Yada Yada
+      </div>
+      <img
+        src="demo_phone.webp"
+        alt="Demo Phone"
+        className="pointer-events-none select-none"
+      />
+      <div className="flex flex-col justify-between gap-40">
+        <div className="rotate-10 rounded-4xl bg-background p-5 font-bold">
+          Placholder
+          <br /> Blah blah blah
+          <br /> Yada Yada Yada Yada Yada
+        </div>
+        <div className="rotate-10 rounded-4xl bg-background p-5 font-bold">
+          Placholder
+          <br /> Blah blah blah
+          <br /> Yada Yada Yada Yada Yada
+        </div>
+      </div>
+    </div>
+  );
+}
