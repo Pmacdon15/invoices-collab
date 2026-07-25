a4c0ac0802ee3e842b5289864a159a303fc20563
Abdul Karim <karim.develops@gmail.com>
2026-07-25 17:05:24 +0500

    style: update underline pos

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,25 +5,21 @@ import SecondaryLink from "@/components/shared/secondary-link";
 export default function Hero() {
   return (
     <div className="flex min-h-[80vh] items-center justify-center gap-40">
-      <div className="flex max-w-115 flex-col gap-15">
-        <h1 className="font-normal font-serif text-7xl text-primary">
-          All Invoices
+      <div className="flex max-w-115 flex-col gap-10">
+        <h1 className="font-normal font-serif text-7xl text-foreground">
+          All <CurveUnderLine text="Invoices" />
           <br />
-          In <CurveUnderLine text="One Place." />
+          In One Place.
         </h1>
-        <div className="flex flex-col gap-10">
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi quae
-            dolorum et, quaerat ex similique fugiat minima, eius sint
-            accusantium!
-          </p>
-          <div className="flex items-center gap-5">
-            <PrimaryLink link="signin" text="Open an account" />
-            <SecondaryLink link="#" text="See how it works" />
-          </div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi quae
+          dolorum et, quaerat ex similique fugiat minima, eius sint accusantium!
+        </p>
+        <div className="flex items-center gap-5">
+          <PrimaryLink link="signin" text="Open an account" />
+          <SecondaryLink link="#" text="See how it works" />
         </div>
       </div>
-
       <div>
         <img
           src={"jar.webp"}
--- components/shared/curve-underline.tsx
+++ components/shared/curve-underline.tsx
@@ -1,13 +1,14 @@
 export function CurveUnderLine({ text }: { text: string }) {
   return (
-    <span className="relative inline-block">
+    <span className="relative inline-block text-primary">
       {text}
       <svg
-        className="pointer-events-none absolute -bottom-10 left-0 w-full"
+        className="pointer-events-none absolute -bottom-4 left-0 w-full"
         viewBox="0 0 200 20"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
+        <title>Curved Underline</title>
         <path
           d="M0 10 Q50 20 100 10 Q150 0 200 10"
           stroke="var(--foreground-warm)"
