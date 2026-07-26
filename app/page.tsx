33cbe611e8a607360c53eeb15873f15ce2648446
Abdul Karim <karim.develops@gmail.com>
2026-07-26 14:24:05 +0500

    style: decrease gap

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import Demo from "@/components/home/Demo";
 import Hero from "@/components/home/Hero";
 import HighlightBar from "@/components/home/HighlightBar";
+import Process from "@/components/home/Process";
 import WhyUs from "@/components/home/WhyUs";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-30">
+    <div className="flex flex-col gap-20">
       <Hero />
       <div className="flex flex-col">
         <Demo />
         <HighlightBar />
         <WhyUs />
       </div>
+      <Process />
     </div>
   );
 }
