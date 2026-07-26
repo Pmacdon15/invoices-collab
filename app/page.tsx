c1deb21ecd3eff13663520e16faa50820306daf1
Abdul Karim <karim.develops@gmail.com>
2026-07-26 14:41:00 +0500

    feat: add a quotes section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Demo from "@/components/home/Demo";
 import Hero from "@/components/home/Hero";
 import HighlightBar from "@/components/home/HighlightBar";
 import Process from "@/components/home/Process";
+import QuoteRec from "@/components/home/QuoteRec";
 import WhyUs from "@/components/home/WhyUs";
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
         <WhyUs />
       </div>
       <Process />
+      <QuoteRec />
     </div>
   );
 }
