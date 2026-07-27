451862761a65bff039ba0900388669ae524fe7f1
Abdul Karim <karim.develops@gmail.com>
2026-07-27 16:02:17 +0500

    fix: replace image with next Image tag

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
 import { CurveUnderLine } from "@/components/shared/curve-underline";
 import PrimaryLink from "@/components/shared/primary-link";
 import SecondaryLink from "@/components/shared/secondary-link";
 
 export default function Hero() {
   return (
-    <div className="flex min-h-[80vh] flex-col items-center justify-center gap-20 p-5 md:flex-row md:gap-40 md:p-10">
+    <div className="flex min-h-[80vh] flex-col items-center justify-center gap-20 p-5 md:flex-row md:gap-40">
       <div className="flex max-w-115 flex-col gap-4">
         <h1 className="font-normal font-serif text-5xl text-foreground leading-20 md:text-7xl md:leading-25">
           All <CurveUnderLine text="Invoices" />
@@ -21,9 +22,11 @@ export default function Hero() {
         </div>
       </div>
       <div>
-        <img
-          src={"jar.webp"}
-          className="pointer-events-none max-w-50 animate-wiggle select-none"
+        <Image
+          src={"/jar.webp"}
+          width={300}
+          height={500}
+          className="pointer-events-none h-75 w-auto animate-wiggle select-none"
           alt="jar with coins"
         />
       </div>
