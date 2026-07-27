1e55dfc3d132ca9c5cd955710e7befb9cb5f0f49
Abdul Karim <karim.develops@gmail.com>
2026-07-27 16:15:30 +0500

    feat: add navbar only for home

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,23 @@ import HighlightBar from "@/components/home/HighlightBar";
 import Process from "@/components/home/Process";
 import QuoteRec from "@/components/home/QuoteRec";
 import WhyUs from "@/components/home/WhyUs";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-20">
-      <Hero />
-      <div className="flex flex-col">
-        <Demo />
-        <HighlightBar />
-        <WhyUs />
+    <>
+      <Navbar />
+      <div className="flex flex-col gap-20">
+        <Hero />
+        <div className="flex flex-col">
+          <Demo />
+          <HighlightBar />
+          <WhyUs />
+        </div>
+        <Process />
+        <QuoteRec />
+        <GetStarted />
       </div>
-      <Process />
-      <QuoteRec />
-      <GetStarted />
-    </div>
+    </>
   );
 }
---
+++ components/home/Navbar.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import SecondaryLink from "@/components/shared/secondary-link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-around border-border border-b-2 px-4 py-4 font-medium md:px-8">
+      <Link href="/" className="flex items-center gap-2">
+        <Image src="/logo.png" alt="logo" width={24} height={24} />
+        <span className="font-bold">VivaPro</span>
+      </Link>
+      <ul className="flex gap-8 *:duration-200 *:hover:-translate-y-0.5">
+        <li>
+          <Link href="#demo">Demo</Link>
+        </li>
+        <li>
+          <Link href="#about">About</Link>
+        </li>
+        <li>
+          <Link href="#process">Process</Link>
+        </li>
+        <li>
+          <Link href="/pricing">Pricing</Link>
+        </li>
+      </ul>
+      <SecondaryLink text="Sign in" link="/signin" />
+    </nav>
+  );
+}
