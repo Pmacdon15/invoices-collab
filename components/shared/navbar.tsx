5d3733f3ed865ba3191e464332461cd40ba75607
Abdul Karim <karim.develops@gmail.com>
2026-07-29 01:14:53 +0500

    fix: wrap in suspense to resolve build errors

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -4,8 +4,10 @@ import { Show, UserButton } from "@clerk/nextjs";
 import { GripHorizontal } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { Suspense } from "react";
 import PrimaryLink from "@/components/shared/primary-link";
 import SecondaryLink from "@/components/shared/secondary-link";
+import Spinner from "@/components/shared/spinner";
 
 export default function Navbar() {
   return (
@@ -14,36 +16,38 @@ export default function Navbar() {
         <Image src="/logo.png" alt="logo" width={32} height={32} />
         <span className="font-bold">VivaPro</span>
       </Link>
-      <Show when={"signed-out"}>
-        <ul className="hidden items-center gap-8 *:duration-200 *:hover:-translate-y-0.5 md:flex">
-          <li>
-            <Link href="/#about">About</Link>
-          </li>
-          <li>
-            <Link href="/#works">How it works</Link>
-          </li>
-          <li>
-            <Link href="/pricing">Pricing</Link>
-          </li>
-          <li>
-            <GripHorizontal />
-          </li>
-          <li>
-            <SecondaryLink text="Sign in" link="/sign-in" />
-          </li>
-          <li>
-            <PrimaryLink text="Get Started" link="/sign-up" size="small" />
-          </li>
-        </ul>
-      </Show>
-      <Show when={"signed-in"}>
-        <span className="md:hidden">
-          <UserButton showName={false} />
-        </span>
-        <span className="hidden md:block">
-          <UserButton showName={true} />
-        </span>
-      </Show>
+      <Suspense fallback={<Spinner />}>
+        <Show when={"signed-out"}>
+          <ul className="hidden items-center gap-8 *:duration-200 *:hover:-translate-y-0.5 md:flex">
+            <li>
+              <Link href="/#about">About</Link>
+            </li>
+            <li>
+              <Link href="/#works">How it works</Link>
+            </li>
+            <li>
+              <Link href="/pricing">Pricing</Link>
+            </li>
+            <li>
+              <GripHorizontal />
+            </li>
+            <li>
+              <SecondaryLink text="Sign in" link="/sign-in" />
+            </li>
+            <li>
+              <PrimaryLink text="Get Started" link="/sign-up" size="small" />
+            </li>
+          </ul>
+        </Show>
+        <Show when={"signed-in"}>
+          <span className="md:hidden">
+            <UserButton showName={false} />
+          </span>
+          <span className="hidden md:block">
+            <UserButton showName={true} />
+          </span>
+        </Show>
+      </Suspense>
     </nav>
   );
 }
