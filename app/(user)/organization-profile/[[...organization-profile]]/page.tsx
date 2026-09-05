ed2f14010a459329c2536ee2e4c23352d9c84e1b
Patrick MacDonald <patrick@patmac.ca>
2026-09-05 15:35:01 -0600

    feat: wrap OrganizationProfile and NavLink components in Suspense for improved loading state

--- app/(user)/organization-profile/[[...organization-profile]]/page.tsx
+++ app/(user)/organization-profile/[[...organization-profile]]/page.tsx
@@ -1,9 +1,13 @@
 import { OrganizationProfile } from "@clerk/nextjs";
+import { Suspense } from "react";
+import Spinner from "@/components/shared/spinner";
 
 export default function OrganizationProfilePage() {
   return (
     <div className="flex min-h-[calc(100vh-5rem)] items-center justify-center p-4">
-      <OrganizationProfile routing="path" path="/organization-profile" />
+      <Suspense fallback={<Spinner />}>
+        <OrganizationProfile routing="path" path="/organization-profile" />
+      </Suspense>
     </div>
   );
 }
